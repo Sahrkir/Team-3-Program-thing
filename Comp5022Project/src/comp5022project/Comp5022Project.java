6b109444ff58741385e64870dd016df46e7679c8
theddon <49587883+theddon@users.noreply.github.com>
2019-11-20 14:18:59 +0000

    Added basic user interface

--- Comp5022Project/src/comp5022project/Comp5022Project.java
+++ Comp5022Project/src/comp5022project/Comp5022Project.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package comp5022project;
+import java.util.Scanner;
 
 /**
  *
@@ -15,6 +16,41 @@ public class Comp5022Project {
      * @param args the command line arguments
      */
     public static void main(String[] args){
-    }
-    
+        boolean menuActive = true;
+        Scanner in = new Scanner(System.in);  
+        User user = new User("Test", 0.0, 0.0);
+        System.out.println("To set income type 'SetInc'. To set expenses type 'SetExp'."
+                + "To add to income type 'AddInc'. To add to expenses type 'AddExp'."
+                + "To show income type 'GetInc'. To show expenses type 'GetExp'.");
+        while(menuActive){
+            System.out.println("Please enter a command.");
+            String input = in.nextLine();
+            switch(input){
+                case "SetInc":
+                    System.out.println("Enter your income");
+                    user.setIncome(new Double(in.nextLine()));
+                    break;
+                case "SetExp":
+                    System.out.println("Enter your expenses");
+                    user.setExpenses(new Double(in.nextLine()));
+                    break;
+                case "AddInc":
+                    System.out.println("Enter the income to add");
+                    user.addIncome(new Double(in.nextLine()));
+                    break;
+                case "AddExp":
+                    System.out.println("Enter the expenses to add");
+                    user.addExpenses(new Double(in.nextLine()));
+                    break;
+                case "GetInc":
+                    System.out.println("Income: "+user.getIncome());
+                    break;
+                case "GetExp":
+                    System.out.println("Expenses: "+user.getExpenses());
+                    break;
+                default:
+                    break;
+            }
+        } 
+    }    
 }
